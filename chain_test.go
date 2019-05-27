6d89ce0add52889ee95a810f80b6d6e2328569c3
casterlee <lxzan520@qq.com>
2019-05-27 18:45:47 +0800

    support multi chains

--- chain_test.go
+++ chain_test.go
@@ -2,7 +2,6 @@ package chainpot
 
 import (
 	"encoding/json"
-	"github.com/fadeAce/claws"
 	"github.com/fadeAce/claws/types"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -14,25 +13,19 @@ import (
 )
 
 func TestNewChainpot(t *testing.T) {
-	cfg, err := ioutil.ReadFile("./test/claws.yml")
+	cfg, err := ioutil.ReadFile("./claws.yml")
 	if cfg == nil || err != nil {
 		panic("shut down with no configuration")
 		return
 	}
 	var conf types.Claws
 	err = yaml.Unmarshal(cfg, &conf)
-	claws.SetupGate(&conf)
-	wallet := claws.Builder.BuildWallet("eth")
-
-	var opt = &ChainOption{
-		ConfirmTimes: 7,
-		Chain:        "eth",
-	}
-	var chainpot = NewChainpot(opt, wallet)
-	chainpot.Add([]string{"0x78aE889cd04Cb9274C2600d68CCc5058F43dB63e"})
-	chainpot.OnMessage = func(msg *PotEvent) {
+	var cp = NewChainpot(&conf)
+	cp.Register("eth", 0)
+	cp.Add(0, []string{"0x78aE889cd04Cb9274C2600d68CCc5058F43dB63e"})
+	cp.OnMessage = func(idx int, msg *PotEvent) {
 		b, _ := json.Marshal(msg)
-		println(string(b))
+		println(idx, string(b))
 	}
 
 	quit := make(chan os.Signal)
--- chainpot.go
+++ chainpot.go
@@ -1,212 +1,46 @@
 package chainpot
 
 import (
-	"context"
-	"fmt"
-	"github.com/fadeAce/chainpot/queue"
 	"github.com/fadeAce/claws"
 	"github.com/fadeAce/claws/types"
-	"math/big"
-	"strconv"
-	"sync"
-	"sync/atomic"
-	"time"
 )
-
-type EventType int
-
-const (
-	// NORMAL STATE
-	T_DEPOSIT EventType = iota
-	T_WITHDRAW
-	T_DEPOSIT_UPDATE
-	T_WITHDRAW_UPDATE
-	T_WITHDRAW_CONFIRM
-	T_DEPOSIT_CONFIRM
-
-	// ABNORMAL STATE
-	T_WITHDRAW_FAIL
-	T_ERROR
-)
-
-type PotEvent struct {
-	Chain   string
-	Event   EventType
-	ID      int64
-	Content types.TXN
-}
 
 type Chainpot struct {
-	*sync.Mutex
-	addrs           map[string]bool
-	currentBlocks   map[int64]int64
-	config          *ChainOption
-	height          int64
-	handledEndPoint bool
-	wallet          claws.Wallet
-	depositTxs      *queue.Queue
-	withdrawTxs     *queue.Queue
-	OnMessage       func(msg *PotEvent)
+	chains    []*Chain
+	conf      map[string]*chainOption
+	OnMessage func(idx int, event *PotEvent)
 }
 
-type ChainOption struct {
-	ConfirmTimes int64
-	Chain        string
-	Endpoint     int64
-}
-
-func NewChainpot(opt *ChainOption, wallet claws.Wallet) *Chainpot {
-	chain := &Chainpot{
-		Mutex:         &sync.Mutex{},
-		addrs:         make(map[string]bool),
-		currentBlocks: make(map[int64]int64),
-		config:        opt,
-		wallet:        wallet,
-		depositTxs:    queue.NewQueue(),
-		withdrawTxs:   queue.NewQueue(),
+func NewChainpot(conf *types.Claws) *Chainpot {
+	var obj = &Chainpot{
+		chains: make([]*Chain, 128),
+		conf:   make(map[string]*chainOption),
 	}
-	go func() {
-		chain.wallet.NotifyHead(context.Background(), func(num *big.Int) {
-			var height = num.Int64()
-			if height > chain.height {
-				atomic.StoreInt64(&chain.height, height)
-			}
-			chain.handleEndpoint(chain.config.Endpoint, height)
-			chain.handleBlock(num)
-		})
-	}()
-
-	go SetInterval(180*time.Second, func() {
-		chain.Lock()
-		var now = time.Now().UnixNano() / 1000000
-		for k, v := range chain.currentBlocks {
-			if now-v > 180000 {
-				delete(chain.currentBlocks, k)
-			}
+	claws.SetupGate(conf)
+	for _, cfg := range conf.Coins {
+		obj.conf[cfg.CoinType] = &chainOption{
+			Chain:        cfg.CoinType,
+			ConfirmTimes: 7,
 		}
-		chain.Unlock()
-	})
-	return chain
+	}
+	return obj
 }
 
-func (c *Chainpot) handleBlock(num *big.Int) {
-	var height = num.Int64()
-	if _, exist := c.currentBlocks[height]; exist {
-		return
-	}
-	txns, err := c.wallet.UnfoldTxs(context.Background(), num)
-	if err != nil {
+func (c *Chainpot) Register(chainName string, endpoint int64) {
+	var opt, exist = c.conf[chainName]
+	if !exist {
 		return
 	}
 
-	c.Lock()
-	c.currentBlocks[height] = time.Now().UnixNano() / 1000000
-	for i, _ := range txns {
-		var tx = txns[i]
-		var _, f1 = c.addrs[tx.FromStr()]
-		var _, f2 = c.addrs[tx.ToStr()]
-		var node = &queue.Value{TXN: tx, Height: height, Index: int64(i)}
-		if (f1 || f2) && tx.FromStr() == tx.ToStr() {
-			c.OnMessage(&PotEvent{
-				Chain: c.config.Chain,
-				ID:    getEventID(height, T_ERROR, int64(i)),
-				Event: T_ERROR,
-			})
-		} else if f1 && f2 {
-			c.withdrawTxs.PushBack(node)
-			var cp = *node
-			c.depositTxs.PushBack(&cp)
-		} else if f1 {
-			c.withdrawTxs.PushBack(node)
-		} else if f2 {
-			c.depositTxs.PushBack(node)
-		}
-	}
-	c.emitter()
-	c.Unlock()
-}
-
-func (c *Chainpot) handleEndpoint(lastHeight int64, latestHeight int64) {
-	if c.handledEndPoint || c.config.Endpoint == 0 {
-		return
-	}
-
-	for i := lastHeight; i < latestHeight; i++ {
-		c.handleBlock(big.NewInt(i))
-	}
-	c.handledEndPoint = true
-}
-
-// emit events
-func (c *Chainpot) emitter() {
-	var m = c.depositTxs.Len()
-	for i := 0; i < m; i++ {
-		var val = c.depositTxs.Front()
-		var msg = &PotEvent{
-			Chain:   c.config.Chain,
-			Content: val.TXN,
-		}
-
-		if !c.wallet.Seek(val.TXN) {
-			continue
-		}
-
-		if c.height-val.Height+1 >= c.config.ConfirmTimes {
-			msg.Event = T_DEPOSIT_CONFIRM
-		} else if c.height-val.Height == 0 {
-			msg.Event = T_DEPOSIT
-			c.depositTxs.PushBack(val)
-		} else {
-			msg.Event = T_DEPOSIT_UPDATE
-			c.depositTxs.PushBack(val)
-		}
-		msg.ID = getEventID(val.Height, msg.Event, val.Index)
-		c.OnMessage(msg)
-	}
-
-	var n = c.withdrawTxs.Len()
-	for i := 0; i < n; i++ {
-		var val = c.withdrawTxs.Front()
-		var msg = &PotEvent{
-			Chain:   c.config.Chain,
-			Content: val.TXN,
-		}
-
-		if !c.wallet.Seek(val.TXN) {
-			msg.Event = T_WITHDRAW_FAIL
-		} else if c.height-val.Height+1 >= c.config.ConfirmTimes {
-			msg.Event = T_WITHDRAW_CONFIRM
-		} else if c.height-val.Height == 0 {
-			msg.Event = T_WITHDRAW
-			c.withdrawTxs.PushBack(val)
-		} else {
-			msg.Event = T_WITHDRAW_UPDATE
-			c.withdrawTxs.PushBack(val)
-		}
-		msg.ID = getEventID(val.Height, msg.Event, val.Index)
-		c.OnMessage(msg)
+	opt.Endpoint = endpoint
+	var wallet = claws.Builder.BuildWallet(opt.Chain)
+	var chain = newChain(opt, wallet)
+	c.chains[opt.IDX] = chain
+	chain.onMessage = func(msg *PotEvent) {
+		c.OnMessage(opt.IDX, msg)
 	}
 }
 
-func (c *Chainpot) Add(addrs []string) (height int64) {
-	c.Lock()
-	for i, _ := range addrs {
-		addr := addrs[i]
-		c.addrs[addr] = true
-	}
-	c.Unlock()
-	return c.height
-}
-
-func getEventID(height int64, event EventType, idx int64) int64 {
-	var str = fmt.Sprintf("%d%03d%06d", height, event, idx)
-	num, _ := strconv.Atoi(str)
-	return int64(num)
-}
-
-func SetInterval(d time.Duration, cb func()) {
-	for {
-		cb()
-		time.Sleep(d)
-	}
+func (c *Chainpot) Add(idx int, addrs []string) (height int64) {
+	return c.chains[idx].add(addrs)
 }
