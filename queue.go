f92cefd7435f7a7f95e16fcf52398da4fd2f9068
terrill <314156936@qq.com>
2019-07-09 15:20:56 +0800

    safety suite[1/3]

--- queue.go
+++ queue.go
@@ -2,6 +2,10 @@ package chainpot
 
 import "github.com/fadeAce/claws/types"
 
+const (
+	HEAD = "_head"
+)
+
 type Value struct {
 	TXN        types.TXN
 	Height     int64
@@ -36,6 +40,40 @@ func (q *Queue) Pop() *Value {
 
 // safe with wrapped pop
 func (q *Queue) PopEach(f func(i int, v *Value)) {
+	var m = q.Len()
+	for i := 0; i < m; i++ {
+		f(i, q.Pop())
+	}
+}
+
+// type SafeQueue
+
+type SafeQueue struct {
+	data []*Value
+
+	// storage based kv tx based safe queue
+
+	// head is a string key represent head value
+	head string
+}
+
+func (q *SafeQueue) Len() int {
+	return len(q.data)
+}
+
+func (q *SafeQueue) Pend(v *Value) {
+	q.data = append(q.data, v)
+}
+
+// todo: it occurs panic when no value's left
+func (q *SafeQueue) Pop() *Value {
+	var val = q.data[0]
+	q.data = q.data[1:q.Len()]
+	return val
+}
+
+// safe with wrapped pop
+func (q *SafeQueue) PopEach(f func(i int, v *Value)) {
 	var m = q.Len()
 	for i := 0; i < m; i++ {
 		f(i, q.Pop())
--- queue_test.go
+++ queue_test.go
@@ -1,6 +1,8 @@
 package chainpot
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -47,4 +49,61 @@ func TestNewQueueNil(t *testing.T) {
 		var cur = q.Pop()
 		println(cur.Index)
 	}
+}
+
+func TestSerializeInterface(t *testing.T) {
+	a := &BlockMessage{
+		Hash:   "a",
+		From:   "b",
+		To:     "c",
+		Fee:    "d",
+		Amount: "e",
+	}
+	b := &BlockMessage{
+		Hash:   "q",
+		From:   "w",
+		To:     "e",
+		Fee:    "r",
+		Amount: "t",
+	}
+	va := &Value{
+		TXN:        a,
+		Height:     1,
+		Index:      2,
+		EventID:    3,
+		IsOldBlock: true,
+	}
+	vb := &Value{
+		TXN:        b,
+		Height:     1,
+		Index:      2,
+		EventID:    3,
+		IsOldBlock: false,
+	}
+	sa, err := json.Marshal(va)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(sa))
+	sb, err := json.Marshal(vb)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(sb))
+
+	// it's time for cover back
+	var va2 = new(Value)
+	va2.TXN = &BlockMessage{}
+	err = json.Unmarshal(sa, va2)
+	var vb2 = new(Value)
+	err = json.Unmarshal(sb, vb2)
+
+
+	a2 := va2.TXN.(*BlockMessage)
+	fmt.Println(a2)
+	b2 := vb2.TXN.(*BlockMessage)
+	fmt.Println(b2)
+
+	fmt.Println(va2.TXN.HexStr())
+	fmt.Println(vb2.TXN.HexStr())
 }
